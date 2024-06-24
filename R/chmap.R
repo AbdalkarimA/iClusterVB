f0d19182aee0864c76d331af4140326f4ac93898
Abdalkarim Alnajjar <alnajjar.ak@gmail.com>
2024-06-24 19:45:20 -0400

    Updated

--- R/chmap.R
+++ R/chmap.R
@@ -5,79 +5,87 @@
 #' @param nvars A numeric vector or a single value. The number of variables to show; defaults to all. Only works for VS_method = 0.
 #' @param title A character vector or a single value. Title of the heat map; defaults to "View 1,..R - Distribution".
 #' @param cols A vector of colors to use for the clusters; defaults to a random selection of colors.
+#' @export
+#' @import pheatmap
 #' @return Returns a heat map.
 #' @examples
-#' chmap(fit_iClusterVB, nvars = 50, title = c("View 1", "View 2"), cols = c("green","blue","purple", "red"))
+#' chmap(fit_iClusterVB, nvars = 50, title = c("View 1", "View 2"), cols = c("green", "blue", "purple", "red"))
 #' chmap(fit_iClusterVB)
+#'
+#' @useDynLib iClusterVB, .registration=TRUE
+
 
 chmap <- function(fit, rho = 0, nvars = NULL, title = NULL, cols = NULL) {
-
-  if(!is.null(nvars) & length(nvars) == 1) {
+  if (!is.null(nvars) & length(nvars) == 1) {
     nvars <- rep(nvars, length(fit$mydata))
   }
 
-  if(!is.null(title) & length(title) == 1) {
+  if (!is.null(title) & length(title) == 1) {
     title <- rep(title, length(fit$mydata))
   }
 
-  if(is.null(nvars)) {
+  if (is.null(nvars)) {
     nvars <- unlist(fit$data_summary[[3]])
   }
 
-  if(is.null(cols)) {
+  if (is.null(cols)) {
     cols <- colors()[sample(1:600, size = length(unique(fit$cluster)))]
   }
 
-  ifelse(is.null(title), title <-paste("View", 1:length(fit$mydata), "-",
-                                       tools::toTitleCase(fit$dist[1:length(fit$mydata)])),
-         title)
+  ifelse(is.null(title), title <- paste(
+    "View", 1:length(fit$mydata), "-",
+    tools::toTitleCase(fit$dist[1:length(fit$mydata)])
+  ),
+  title
+  )
 
-  if(is.null(fit$model_parameters$rho)) {
+  if (is.null(fit$model_parameters$rho)) {
     for (i in 1:length(fit$mydata)) {
-      df <- as.data.frame(t(matrix(as.numeric(unlist(fit$mydata[[i]])), nrow = nrow(fit$mydata[[i]]))[,1:nvars[i]]) )
+      df <- as.data.frame(t(matrix(as.numeric(unlist(fit$mydata[[i]])), nrow = nrow(fit$mydata[[i]]))[, 1:nvars[i]]))
       mat_col <- data.frame(Clusters = as.numeric(fit$cluster))
       rownames(mat_col) <- colnames(df)
       df <- df[, order(as.numeric(fit$cluster))]
       mat_colors <- list(Clusters = cols)
       pheatmap::pheatmap(df,
-                         main = as.character(title[i]),
-                         cluster_rows = FALSE,
-                         cluster_cols = FALSE,
-                         color = colorRampPalette(c("navy", "white", "firebrick3"))(50),
-                         treeheight_row=0,
-                         treeheight_col=0,
-                         scale = "row",
-                         show_colnames = FALSE,
-                         show_rownames = FALSE,
-                         annotation_names_row = FALSE,
-                         annotation_names_col = FALSE,
-                         annotation_col   = mat_col,
-                         annotation_colors= mat_colors)
-
+        main = as.character(title[i]),
+        cluster_rows = FALSE,
+        cluster_cols = FALSE,
+        color = colorRampPalette(c("navy", "white", "firebrick3"))(50),
+        treeheight_row = 0,
+        treeheight_col = 0,
+        scale = "row",
+        show_colnames = FALSE,
+        show_rownames = FALSE,
+        annotation_names_row = FALSE,
+        annotation_names_col = FALSE,
+        annotation_col = mat_col,
+        annotation_colors = mat_colors
+      )
     }
-  } else if(!is.null(fit$model_parameters$rho)) {
+  } else if (!is.null(fit$model_parameters$rho)) {
     for (i in 1:length(fit$mydata)) {
-      df <- as.data.frame(t(matrix(as.numeric(unlist(fit$mydata[[i]][, fit$model_parameters$rho[[i]] >= rho]  )),
-                                   nrow = nrow(fit$mydata[[i]][, fit$model_parameters$rho[[i]] >= rho]))))
+      df <- as.data.frame(t(matrix(as.numeric(unlist(fit$mydata[[i]][, fit$model_parameters$rho[[i]] >= rho])),
+        nrow = nrow(fit$mydata[[i]][, fit$model_parameters$rho[[i]] >= rho])
+      )))
       mat_col <- data.frame(Clusters = as.numeric(fit$cluster))
       rownames(mat_col) <- colnames(df)
       df <- df[, order(as.numeric(fit$cluster))]
       mat_colors <- list(Clusters = cols)
       pheatmap::pheatmap(df,
-                         main = as.character(title[i]),
-                         cluster_rows = FALSE,
-                         cluster_cols = FALSE,
-                         color = colorRampPalette(c("navy", "white", "firebrick3"))(50),
-                         treeheight_row=0,
-                         treeheight_col=0,
-                         scale = "row",
-                         show_colnames = FALSE,
-                         show_rownames = FALSE,
-                         annotation_names_row = FALSE,
-                         annotation_names_col = FALSE,
-                         annotation_col   = mat_col,
-                         annotation_colors= mat_colors)
-
+        main = as.character(title[i]),
+        cluster_rows = FALSE,
+        cluster_cols = FALSE,
+        color = colorRampPalette(c("navy", "white", "firebrick3"))(50),
+        treeheight_row = 0,
+        treeheight_col = 0,
+        scale = "row",
+        show_colnames = FALSE,
+        show_rownames = FALSE,
+        annotation_names_row = FALSE,
+        annotation_names_col = FALSE,
+        annotation_col = mat_col,
+        annotation_colors = mat_colors
+      )
     }
   }
 }
--- R/summary.iClusterVB.R
+++ R/summary.iClusterVB.R
@@ -3,26 +3,30 @@
 #' @param object A fitted iClusterVB object.
 #' @param vs_prob Input to count the number of variables that meet the minimum probability of inclusion vs_prob. Default is 0.5. Only works for VS_method = 1.
 #' @return Returns a summary showing the initialization method, the cluster memberships, and the number of variables for which the probability of inclusion is above vs_prob
+#' @export
 #' @examples
 #' summary(fit_iClusterVB)
+#'
+#' @useDynLib iClusterVB, .registration=TRUE
 
 
 summary.iClusterVB <- function(object, vs_prob = NULL, ...) {
-
-  if(is.null(vs_prob)) {
+  if (is.null(vs_prob)) {
     vs_prob <- 0.5
   }
 
   mybiglist <- list()
 
   for (i in 1:length(object$mydata)) {
-    name <- paste(object$dist[i])
+    name <- paste("View", i, "-", object$dist[i], sep = " ")
     mybiglist[[name]] <- sum(object$model_parameters$rho[[i]] >= vs_prob)
   }
 
 
-  summ <- list("Initialization Method" = object$initial_values$initial_method,
-               "Cluster Memberships" = table(object$cluster),
-               "# Selected variables above vs_prob" = mybiglist )
+  summ <- list(
+    "Initialization Method" = object$initial_values$initial_method,
+    "Cluster Memberships" = table(object$cluster),
+    "# Selected variables above vs_prob" = mybiglist
+  )
   summ
 }
